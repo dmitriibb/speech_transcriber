a6bbd1f3cfca179d5d0aa533fb587845e2878a5c
dmitrii.b <dmitriibb616@gmail.com>
2025-05-25 15:21:32 +0200

    fixed

--- src/AudioDeviceWrapper.py
+++ src/AudioDeviceWrapper.py
@@ -3,11 +3,16 @@ from src.constants import deviceTypeInput, deviceTypeOutput
 
 class AudioDeviceWrapper:
     def __init__(self, device):
-        self.device = device
+        self.device_info = device
+        self.device_id = device['index']  # Use the device index for sounddevice
         if device['max_input_channels'] > 0:
             self.type = deviceTypeInput
         else:
             self.type = deviceTypeOutput
 
     def get_name(self) -> str:
-        return f"{self.type} - {self.device['name']}"
+        return f"{self.type} - {self.device_info['name']}"
+
+    @property
+    def device(self):
+        return self.device_id  # Return just the device index for sounddevice
